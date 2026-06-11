6ba72853495a21784c150a2f30766fb548381805
oyamairishitomi <h.tezuka0707@gmail.com>
2026-06-11 22:07:06 +0900

    メンターレビューを反映: 不要なJSONレスポンス・ビューの削除と日報の編集・削除ボタンの表示制御

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,10 +26,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: t('controllers.common.notice_create', name: Book.model_name.human) }
-        # format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
-        # format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
